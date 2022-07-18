748d36918639a0778aaa15883a282ed271964c9e
gracechiu <rick.grace.1014@gmail.com>
2022-07-18 13:05:41 +0800

    使用ajax來增加表格 刪除表格

--- app2/api/query.php
+++ app2/api/query.php
@@ -1,5 +1,18 @@
+
+
 <?php
 //處理查詢資料的請求
+// 建立資料庫的連線
+// PDO Json
+//處理查詢資料的請求
+$classroom=$_GET['classroom'];
+$dsn="mysql:host=localhost;charset=utf8;dbname=students";
+$pdo=new PDO($dsn,'root','');
 
+$sql="select * from students where classroom='{$classroom}'";
+// $sql="insert into students (`classroom`, ``)";
+$rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($rows);
 
 ?>
